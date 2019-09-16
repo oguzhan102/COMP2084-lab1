c89c690b5dc6b2263e474a3f7d84d03e1689a6a3
oğuzhan küçükoğlu <oguzhan102201@hotmail.com>
2019-09-16 18:52:56 -0400

    initial commit

---
+++ COMP2084-lab1/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using COMP2084_lab1.Models;
+using System.Web;
+
+namespace COMP2084_lab1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public String Addition(int x, int y)
+        {
+            int result = x + y;
+            String result3 = ("Result= " + result);
+            return result3;
+        }
+
+        public String multiplication(int x, int y)
+        {
+            int result2 = x * y;
+            String result4 = ("Result =" + result2);
+            return result4;
+        }
+
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
